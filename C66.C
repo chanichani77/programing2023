c2035c2741e0bbb6222eb65d9c40af455702c21c
chanichani77 <133177353+chanichani77@users.noreply.github.com>
2023-05-11 10:01:21 +0900

    c언어

---
+++ C66.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// #define PI 3.14159265
+int main(){
+    int r=5;
+    const double PI=3.14159265;
+    double cir = 2*r*PI;
+    printf("radius:%d,circumference:%1f\n",r,cir);
+
+    return 0;
+}
